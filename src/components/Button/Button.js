2bef65f31bf726c1b7901a20797c06e74d6fd2e2
VelPaulov <velyakishkin@gmail.com>
2022-11-06 14:02:10 +0300

    Updated Button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,24 +1,23 @@
-import './button.scss';
 import { Component } from "../../core";
+
+import './button.scss';
 
 export class Button extends Component {
     constructor() {
         super();
         this.state = {
             isActive: false,
-            label: 'click',
+            label: 'Click',
             count: 1,
-        }
+        };
     }
 
-    registerEvents() {
-        this.addEventListener('click', () => {
-            this.setState((state) => {
-                return {
-                    ...state,
-                    isActive: !state.isActive,
-                };
-            });
+    changeColor() {
+        this.setState((state) => {
+            return {
+                ...state,
+                isActive: !state.isActive,
+            };
         });
     }
 
@@ -26,39 +25,42 @@ export class Button extends Component {
         this.setState((state) => {
             return {
                 ...state,
-                count: (state.count < 10) ? state.count += 1 : state.count,
-            }
-        })
+                count: state.count < 10 ? state.count += 1 : state.count,
+            };
+        });
     }
 
     minus() {
         this.setState((state) => {
             return {
                 ...state,
-                count: (state.count > 0) ? state.count -= 1 : state.count,
-            }
-        })
+                count: state.count > 0 ? state.count -= 1 : state.count,
+            };
+        });
     }
 
     registerEvents() {
         this.addEventListener('click', (evt) => {
-            if(evt.target.closest('.plus')) {
+            if (evt.target.closest('.click')) {
+                this.changeColor();
+            }
+            if (evt.target.closest('.plus')) {
                 this.plus();
             }
-            if(evt.target.closest('.minus')) {
+            if (evt.target.closest('.minus')) {
                 this.minus();
             }
-        })
+        });
     }
-    
+
     render() {
         return `
-            <button class='${this.state.isActive ? 'active' : ''}'>${this.state.label}</button>
+            <button class='click ${this.state.isActive ? 'active' : ''}'>${this.state.label}</button>
             <button class='plus'>+</button>
             <span>${this.state.count}</span>
             <button class='minus'>-</button>
         `
-    };
+    }
 }
 
 customElements.define('my-button', Button);

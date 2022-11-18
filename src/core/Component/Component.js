ef794cb8904e3934d539b81f800b1f888b1857fb
VelPaulov <velyakishkin@gmail.com>
2022-11-18 17:01:38 +0300

    changed Component

--- src/core/Component/Component.js
+++ src/core/Component/Component.js
@@ -7,21 +7,12 @@ export class Component extends HTMLElement {
     
     setState(callback) {
         this.state = callback(this.state);
-        this.innerHTML = this.render()
-        .toString()
-        .trim()
-        .replaceAll(/true|false/gi, "")
-        .replaceAll(",", "");
+        this.innerHTML = this.render();
     }
 
     connectedCallback() {
-        this.innerHTML = this.render()
-            .toString()
-            .trim()
-            .replaceAll(/true|false/gi, "")
-            .replaceAll(",", "");
+        this.innerHTML = this.render();
         this.componentDidMount();
-        this.registerEvents();
     }
 
     disconnectedCallback() {
@@ -32,16 +23,15 @@ export class Component extends HTMLElement {
         this.componentWillUpdate(name, oldValue, newValue);
         this.getAttributeNames().forEach(() => {
             this.props[name] = this.getAttribute(name);
-        })
+        });
     }
 
     dispatch(type, props) {
         this.dispatchEvent(new CustomEvent(type, { bubbles: true, detail: props }));
     }
     
-    registerEvents() {}
-    componentDidMount() {}
-    componentWillUpdate() {}
-    componentWillUnMount() {}
     render() {}
+    componentDidMount() {}
+    componentWillUnMount() {}
+    componentWillUpdate() {}
 }
